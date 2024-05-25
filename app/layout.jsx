4f22f3f4c6fe142b0124e55ee007d0f435c4dec5
Brennam Bond <brennambond@gmail.com>
2024-05-25 18:36:24 -0500

    Add StarBackground component to app

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Source_Code_Pro } from "next/font/google";
 import "./globals.css";
+import { StarsCanvas } from "@/components/shared";
 
 const inter = Source_Code_Pro({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StarsCanvas />
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <main>Test</main>;
+  return (
+    <main className='flex flex-col min-h-screen bg-gradient-to-b from-[#242443] to-purple-950'>
+      Test
+    </main>
+  );
 }
