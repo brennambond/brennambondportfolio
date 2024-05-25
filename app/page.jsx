02729627efbef18abecc28ddece8e9dbb1568285
Brennam Bond <brennambond@gmail.com>
2024-05-25 18:40:09 -0500

    Add intial section components for project

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,24 @@
+import {
+  Hero,
+  Navbar,
+  About,
+  Projects,
+  Contact,
+  Footer,
+} from "@/components/shared";
+
 export default function Home() {
   return (
     <main className='flex flex-col min-h-screen bg-gradient-to-b from-[#242443] to-purple-950'>
-      Test
+      <Navbar />
+
+      <div className='container mx-auto flex-center flex-col gap-40 sm:gap-60 md:gap-80 '>
+        <Hero />
+        <About />
+        <Projects />
+        <Contact />
+      </div>
+      <Footer />
     </main>
   );
 }
--- components/shared/index.js
+++ components/shared/index.js
@@ -1,3 +1,9 @@
 import StarsCanvas from "./StarBackground";
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Projects from "./Projects";
 
-export { StarsCanvas };
+export { StarsCanvas, About, Contact, Footer, Hero, Navbar, Projects };
