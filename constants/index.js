b57279bd2bdb10dc41b53be7f0b1cca5dac051ba
Brennam Bond <brennambond@gmail.com>
2024-05-25 18:44:23 -0500

    Add constants folder and React Icons dependency

---
+++ constants/index.js
@@ -0,0 +1,35 @@
+import {
+  SiReact,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiTailwindcss,
+  SiMongodb,
+  SiPostgresql,
+} from "react-icons/si";
+
+export const myNavLinks = [
+  {
+    title: "About",
+    href: "#about",
+    id: 1,
+  },
+  {
+    title: "Projects",
+    href: "#projects",
+    id: 2,
+  },
+  {
+    title: "Contact",
+    href: "#contact",
+    id: 3,
+  },
+];
+
+export const heroIcons = [
+  SiReact,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiTailwindcss,
+  SiMongodb,
+  SiPostgresql,
+];
