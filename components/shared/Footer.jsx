d82f89977a4fe470fad8cd289742b03992fd6af6
Brennam Bond <brennambond@gmail.com>
2024-05-25 19:16:54 -0500

    Finish Footer section component

--- components/shared/Footer.jsx
+++ components/shared/Footer.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className=' w-full bg-[#1b1b2e] z-50'>
+      <div className='flex items-center justify-between px-5 py-6 container'>
+        <Link
+          href='/'
+          className='flex-center gap-2 transition ease-in-out delay-100 hover:scale-105'
+        >
+          <Image
+            src='/assets/images/logo.png'
+            width={1000}
+            height={1000}
+            className='w-8 h-8 '
+            alt='Logo'
+          />
+          <p className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600 text-2xl  font-semibold block'>
+            PORTFOLIO
+          </p>
+        </Link>
+        <p className='text-white'>All rights reserved</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
